59ed1c46eddff2c0e7dc2b32884cc90f9ff5056f
trandinhthang <thang.tran.5243817@gmail.com>
2020-09-06 14:09:12 +0700

    edit homescreen

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -11,13 +11,13 @@ import books from "../assests/images/books.png";
 
 export default function CategoryList(props) {
     const {onPress}=props;
-    const {category}=props;
+    // const {category}=props;
         return(
             <TouchableOpacity activeOpacity={0.2} onPress={onPress}>
             <View style={Styles.container} >
                 <Image style={Styles.translateImage} source={books}/>
-                <Text style={Styles.title}>{category.name1}</Text>
-                <Text style={Styles.title1}>{category.name}</Text>
+                <Text style={Styles.title}>{props.name1}</Text>
+                <Text style={Styles.title1}>{props.name}</Text>
 
             </View>
             </TouchableOpacity>  
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,44 +1,73 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { 
-    FlatList
+    ScrollView,
+    View,
+    Text,
+    Image,
+    TouchableOpacity
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import CategoryList from "../components/CategoryList";
-//gọi các screens khác từ component categorylist
-export default class HomeScreen extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            categories:[
-            {id:'1', name:'Dịch từ',name1:'Traduire mots'},
-            {id:'2', name:'Từ vựng',name1:'Vocabulaire'},  
-            {id:'3', name:'Ngữ pháp',name1:'Grammaire'}, 
-            {id:'4', name:'Luyện tập',name1:'Grammaire'},           
-            ]   
-        };
-        }
-    render(){
-        const {navigation} =this.props;
-        const {categories} =this.state;
-        return(
-            <FlatList
-            data={categories}
-            renderItem={({item})=> <CategoryList category={item} onPress={() => 
-                navigation.navigate("Translate",{ name: item.name },{name1:item.name1}  
-            )} />}
-            keyExtractor={item => item.id}
-            contentContainerStyle={{backgroundColor:"#e1e1e6",paddingLeft:50,paddingRight:50}}
-            /> 
-        )
-    }
+import Styles from '../styles/Styles';
+import books from "../assests/images/books.png";
+import research from "../assests/images/research.png";
+import vocabulary from "../assests/images/vocabulary.png";
+//gọi các Screen khác từ đây sang App.js
+const HomeScreen =({navigation})=>{
+    return(
+        <ScrollView style={{backgroundColor:"#e1e1e6",paddingLeft:50,paddingRight:50}}>
+            <TouchableOpacity activeOpacity={0.2} onPress={() => navigation.navigate('Translate')}>
+                <View style={Styles.container} >
+                    <Image style={Styles.translateImage} source={research}/>
+                    <Text style={Styles.title}>Traduire mots</Text>
+                    <Text style={Styles.title1}>Dịch từ</Text>
+                </View>
+            </TouchableOpacity> 
+            <TouchableOpacity activeOpacity={0.2} onPress={() => navigation.navigate('Vocabulary')}>
+                <View style={Styles.container} >
+                    <Image style={Styles.translateImage} source={vocabulary}/>
+                    <Text style={Styles.title}>Vocabulaire</Text>
+                    <Text style={Styles.title1}>Từ vựng</Text>
+                </View>
+            </TouchableOpacity>  
+            <TouchableOpacity activeOpacity={0.2} onPress={() => navigation.navigate('Grammar')}>
+                <View style={Styles.container} >
+                    <Image style={Styles.translateImage} source={books}/>
+                    <Text style={Styles.title}>Grammaire</Text>
+                    <Text style={Styles.title1}>Ngữ pháp</Text>
+                </View>
+            </TouchableOpacity> 
+        </ScrollView> 
+);     
 }
-// const {navigation} =this.props;
-//         return(
-//             <ScrollView style={{backgroundColor:"#e1e1e6",paddingLeft:50,paddingRight:50}}>
-//                 <CategoryList name="Dịch từ" name1="Traduire mots" onPress={() => navigation.navigate('Translate')} />
-//                 <CategoryList name="Từ vựng" name1="Vocabulaire" onPress={() => navigation.navigate('Vocabulary')} />
-//                 <CategoryList name="Ngữ pháp" name1="Grammaire" onPress={() => navigation.navigate('Grammar')} />
-//             </ScrollView>
-//         );   
+export default HomeScreen;
+ 
+// constructor(props){
+//     super(props);
+//     this.state={
+//         categories:[
+//         {id:'1', name:'Dịch từ',name1:'Traduire mots'},
+//         {id:'2', name:'Từ vựng',name1:'Vocabulaire'},  
+//         {id:'3', name:'Ngữ pháp',name1:'Grammaire'}, 
+//         {id:'4', name:'Luyện tập',name1:'Grammaire'},           
+//         ]   
+//     };
+//     }
+// render(){
+//     const {navigation} =this.props;
+//     const {categories} =this.state;
+//     return(
+//         <FlatList
+//         data={categories}
+//         renderItem={({item})=> <CategoryList category={item} onPress={() => 
+//             navigation.navigate("Translate",{ name: item.name },{name1:item.name1}  
+//         )} />}
+//         keyExtractor={item => item.id}
+//         contentContainerStyle={{backgroundColor:"#e1e1e6",paddingLeft:50,paddingRight:50}}
+//         /> 
+//     )
+// }
+/* <CategoryList name="Dịch từ" name1=""  />
+<CategoryList name="Từ vựng" name1="Vocabulaire" onPress={() => navigation.navigate('Vocabulary')} />
+<CategoryList name="Ngữ pháp" name1="Grammaire" onPress={() => navigation.navigate('Grammar')} /> */
