724e8db8c463c6b3d485174ce98492806b116bf3
TranDinhThang <thang.tran.5243817@gmail.com>
2021-07-11 11:00:56 +0700

    api fr-vn

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAD1yJjrTvFl95nc4qCytSM5-dPDH6MBKI",
-    authDomain: "feedback-france.firebaseapp.com",
-    databaseURL: "https://feedback-france-default-rtdb.firebaseio.com",
-    projectId: "feedback-france",
-    storageBucket: "feedback-france.appspot.com",
-    messagingSenderId: "631153248396",
-    appId: "1:631153248396:web:0b555694d220f08dbc53a9",
-    measurementId: "G-W7T5QQ2978"
-  };
+  apiKey: 'AIzaSyAD1yJjrTvFl95nc4qCytSM5-dPDH6MBKI',
+  authDomain: 'feedback-france.firebaseapp.com',
+  databaseURL: 'https://feedback-france-default-rtdb.firebaseio.com',
+  projectId: 'feedback-france',
+  storageBucket: 'feedback-france.appspot.com',
+  messagingSenderId: '631153248396',
+  appId: '1:631153248396:web:b8aa3dbbbc17bcbdbc53a9',
+  measurementId: 'G-29ZB7CPQE7',
+};
--- screens/FrenchVietScreen.js
+++ screens/FrenchVietScreen.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Component } from 'react';
-import { View, Text,TextInput ,} from 'react-native';
-import { ScrollView, useWindowDimensions } from "react-native";
+import {Component} from 'react';
+import {View, Text, TextInput} from 'react-native';
+import {ScrollView, useWindowDimensions} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import HTML from "react-native-render-html";
+import HTML from 'react-native-render-html';
 
 const htmlContent = `
     <h1>This HTML snippet is now rendered with native components !</h1>
@@ -14,56 +14,80 @@ const htmlContent = `
 const FrenchVietScreen = () => {
   const contentWidth = useWindowDimensions().width;
   const contentHeight = useWindowDimensions().height;
-  const [word,setWord] = React.useState("")
-  const [data,setData] = React.useState(null)
-  const [loading,setLoading] = React.useState(false)
+  const [word, setWord] = React.useState('');
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
-  async function searchAPI(word){
+  async function searchAPI(word) {
     try {
-      const res = await fetch( `https://api.tracau.vn/WBBcwnwQpV89/df/${word}`)
-      const dataSearch = await res.json()
-      const dataFinal = dataSearch.tratu[0].fields.fulltext
-      setLoading(false)
-      setData(dataFinal)
+      const res = await fetch(`https://fr-vn.herokuapp.com/api/fr-vn/${word}`);
+      const dataSearch = await res.json();
+
+      const dataFinal = dataSearch[0];
+      setLoading(false);
+      setData(dataFinal);
     } catch (error) {
-      console.log(`error`, error)
+      console.log(`error`, error);
     }
   }
   const onSubmit = () => {
-    setData(null)
-    setLoading(true)
-    searchAPI(word)
-  }
-  
+    setData(null);
+    setLoading(true);
+    searchAPI(word);
+  };
+
   return (
     <View>
-         <View style={{
-            flexDirection:'row',
-            justifyContent:"space-between",
-            alignItems:'center',
-            backgroundColor:'white',
-            borderRadius:12,
-            paddingLeft:25,
-            height:53,
-         
-         }}>
-            <TextInput placeholder="Pháp Việt !" 
-            placeholderTextColor="#239dad" 
-            style={{ fontWeight: 'bold', fontSize: 16, width: 255 }} 
-            value={word}
-            onChangeText={setWord} 
-            />
-            <AntDesign onPress={()=> onSubmit()}  name='search1' color='#239dad' size={25} style={{   marginRight:20}} />
-        </View>
-        <View style={{backgroundColor:"white",padding:10, margin:10,height:contentHeight-175}}>
-          <ScrollView  >
-            {loading && (<Text>Loading</Text>)}
-            {data &&( <HTML source={{ html: data }} contentWidth={contentWidth} />) }
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          backgroundColor: 'white',
+          borderRadius: 12,
+          paddingLeft: 25,
+          height: 53,
+        }}>
+        <TextInput
+          placeholder="Pháp Việt !"
+          placeholderTextColor="#239dad"
+          style={{fontWeight: 'bold', fontSize: 16, width: 255}}
+          value={word}
+          onChangeText={setWord}
+        />
+        <AntDesign
+          onPress={() => onSubmit()}
+          name="search1"
+          color="#239dad"
+          size={25}
+          style={{marginRight: 20}}
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: 'white',
+          padding: 10,
+          margin: 10,
+          height: contentHeight - 175,
+        }}>
+        <ScrollView>
+          {loading && <Text>Loading</Text>}
+          {data && (
+            <View>
+              <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                {data.name}
+              </Text>
 
-          </ScrollView>
-        </View>
+              <HTML
+                source={{html: data.textFull}}
+                contentWidth={contentWidth}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default FrenchVietScreen
+export default FrenchVietScreen;
